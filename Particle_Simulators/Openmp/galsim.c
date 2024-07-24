421ac5d6cd190f7024e1c6ea04e8f1dc11c71d5b
Connor Harris <60994277+Connor-H-Bot@users.noreply.github.com>
2024-07-24 09:43:49 +0200

    Update galsim.c

--- Particle_Simulators/Openmp/galsim.c
+++ Particle_Simulators/Openmp/galsim.c
@@ -1,4 +1,4 @@
-// 2D particle simulation by Connor Harris. 
+// 2d particle simulation by Connor Harris. OMP Version
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,143 +6,235 @@
 #include <sys/time.h>
 #include <omp.h>
 
-double plummer_sphere = 1e-3; // Plummer sphere as a part of the force calculation.
+double plummer_sphere = 1e-3;
+char X = 'x', Y = 'y';
+double gravity, delta_time;
+int time_steps, star_count, thread_count;
 
-// Structure for each individual particle
-struct particles {
-    double *position_x, *position_y, *mass, *velocity_x, *velocity_y, *brightness, *accel_x, *accel_y; 
+omp_lock_t lock;
+
+struct particles
+{
+    double *position_x, *position_y, *mass, *velocity_x, *velocity_y, *brightness, *accel_x, *accel_y;
 };
 
-// Function Declarations
 int main(int argc, char const *argv[]);
-double force(int idx, char axis, struct particles* all_particles, int star_count, double gravity);
-double distance(int idx1, int idx2, struct particles* all_particles);
-double vector(int idx1, int idx2, char axis, struct particles* all_particles);
+void calculate_particles(struct particles *all_particles);
+void load_balance_threads(int *thread_ranges_balanced, int length);
+void reset_array(struct particles *all_particles, int start_index, int end_index);
+double force_bidirectional(struct particles *all_particles, char axis, int particle_1, int particle_2);
+double distance(struct particles *all_particles, int index_1, int index_2);
+double vector(struct particles *all_particles, int index_1, int index_2, char axis);
 
-// Calculate force on a particle. Equation: Force(i) = -gravity * mass(i) * sum(mass(j) / (distance(i, j) + plummer_sphere)^3 * vector(i, j))
-double force(int idx, char axis, struct particles* all_particles, int star_count, double gravity) {
-    double summation = 0.0;
-    for (int i = 0; i < star_count; i++) { 
-        if (idx != i) {
-            summation += ((all_particles->mass[i]) / pow((distance(idx, i, all_particles) + plummer_sphere), 3.0)) * vector(idx, i, axis, all_particles);
+/*
+    For each timestep, symmetrically calculate a particles force and
+    use it to update the accelleration, and then velocity and position.
+    The symmetric force calculations are balanced out so that all threads
+    do the same ammount of work, and then they use an even subset of particles
+    for the asymmetric calculations.
+*/
+void calculate_particles(struct particles *all_particles)
+{
+    omp_init_lock(&lock);
+    int *thread_ranges_balanced = (int *)malloc((thread_count + 1) * sizeof(int));
+    load_balance_threads(thread_ranges_balanced, thread_count + 1);
+
+    for (int current_timestep = 0; current_timestep < time_steps; current_timestep++)
+#pragma omp parallel shared(lock)
+    {
+        int thread_id = omp_get_thread_num();
+        int total_threads = omp_get_num_threads();
+        int chunk_size = star_count / total_threads;
+        int start_index = thread_id * chunk_size;
+        int end_index = (thread_id == total_threads - 1) ? star_count : (thread_id + 1) * chunk_size;
+        int start_index_load_balanced = thread_ranges_balanced[thread_id];
+        int end_index_load_balanced = thread_ranges_balanced[thread_id + 1];
+
+        reset_array(all_particles, start_index_load_balanced, end_index_load_balanced);
+#pragma omp barrier
+        double force_x, force_y, x_i, y_i;
+        double local_x_j[star_count];
+        double local_y_j[star_count];
+        memset(local_x_j, 0, star_count * sizeof(double)); // (Leave this in the OMP version or undefined behaviour occurs)
+        memset(local_y_j, 0, star_count * sizeof(double));
+        for (int thread_index = start_index_load_balanced; thread_index < end_index_load_balanced; thread_index++)
+        {
+            x_i = 0.0;
+            y_i = 0.0;
+            for (int i = thread_index; i < star_count; i++)
+            {
+                force_x = force_bidirectional(all_particles, X, thread_index, i);
+                force_y = force_bidirectional(all_particles, Y, thread_index, i);
+                x_i += (force_x * all_particles->mass[i]);
+                y_i += (force_y * all_particles->mass[i]);
+                local_x_j[i] -= (force_x * all_particles->mass[thread_index]);
+                local_y_j[i] -= (force_y * all_particles->mass[thread_index]);
+            }
+            omp_set_lock(&lock);
+            all_particles->accel_x[thread_index] += x_i;
+            all_particles->accel_y[thread_index] += y_i;
+            for (int i = thread_index; i < star_count; i++)
+            {
+                all_particles->accel_x[i] += local_x_j[i];
+                all_particles->accel_y[i] += local_y_j[i];
+                local_x_j[i] = 0.0;
+                local_y_j[i] = 0.0;
+            }
+            omp_unset_lock(&lock);
         }
+#pragma omp barrier
+        for (int thread_index = start_index; thread_index < end_index; thread_index++)
+        {
+            all_particles->accel_x[thread_index] = (-gravity * all_particles->mass[thread_index] * all_particles->accel_x[thread_index]) / all_particles->mass[thread_index];
+            all_particles->accel_y[thread_index] = (-gravity * all_particles->mass[thread_index] * all_particles->accel_y[thread_index]) / all_particles->mass[thread_index];
+        }
+
+        // Calculate a particle segments velocity, then position
+        for (int thread_index = start_index; thread_index < end_index; thread_index++)
+        {
+            all_particles->velocity_x[thread_index] += (delta_time * all_particles->accel_x[thread_index]);
+            all_particles->velocity_y[thread_index] += (delta_time * all_particles->accel_y[thread_index]);
+            all_particles->position_x[thread_index] += (delta_time * all_particles->velocity_x[thread_index]);
+            all_particles->position_y[thread_index] += (delta_time * all_particles->velocity_y[thread_index]);
+        }
+#pragma omp barrier
     }
-    return (-gravity * all_particles->mass[idx] * summation);
+    omp_destroy_lock(&lock);
 }
 
+/*
+    Higher particle numbers require less work due to the symmetrical force property,
+    so this function calculates how many particles each thread should take relative to
+    the work required for calcualting the updated accelleration.
+*/
+inline void load_balance_threads(int *thread_ranges_balanced, int length)
+{
+    int symmetrical_calculations = (star_count * (star_count + 1) / 2); // sum of natural numbers formula
+    int calculations_per_thread = symmetrical_calculations / (length - 1);
+    thread_ranges_balanced[0] = 0;
+    int increment = 0;
+    int decrement = (length - 1);
+    int range = 0;
+
+    for (int i = 0; i < star_count; i++)
+    {
+        range += i;
+        if (range >= (calculations_per_thread * increment))
+        {
+            thread_ranges_balanced[decrement] = (star_count - i);
+            increment++;
+            decrement--;
+            if (increment == (length - 1))
+            {
+                break;
+            }
+        }
+    }
+}
+
+// Reset the accel values of a given particle subset to zero
+inline void reset_array(struct particles *all_particles, int start_index, int end_index)
+{
+    for (int i = start_index; i < end_index; i++)
+    {
+        all_particles->accel_x[i] = 0;
+        all_particles->accel_y[i] = 0;
+    }
+}
+
+// Calculate the reciprocal (dist(i, j) + e3)^3) * vector(i, j) for two particles
+inline double force_bidirectional(struct particles *all_particles, char axis, int particle_1, int particle_2)
+{
+    double dist = distance(all_particles, particle_1, particle_2) + plummer_sphere;
+    double dist_reciprocal = 1.0 / (dist * dist * dist);
+    return (dist_reciprocal * vector(all_particles, particle_1, particle_2, axis));
+}
 
 // Calculate the distance of two points. Distance = Sqrt((x2 - x1)^2 + (y2-y1)^2)
-double distance(int idx1, int idx2, struct particles* all_particles) {
-    return sqrt(pow((all_particles->position_x[idx2] - all_particles->position_x[idx1]), 2.0) + pow((all_particles->position_y[idx2] - all_particles->position_y[idx1]), 2.0));
+inline double distance(struct particles *all_particles, int index_1, int index_2)
+{
+    double x_distance = all_particles->position_x[index_2] - all_particles->position_x[index_1];
+    double y_distance = all_particles->position_y[index_2] - all_particles->position_y[index_1];
+    return sqrt(x_distance * x_distance + y_distance * y_distance);
 }
 
 // Calculate the vector of point A relative to point B in its respective axis. Vector = (x1 - x2, y1 - y2)
-double vector(int idx1, int idx2, char axis, struct particles* all_particles){
-    if (axis == 'x') {
-        return (all_particles->position_x[idx1] - all_particles->position_x[idx2]);
-    }
-    else {
-        return (all_particles->position_y[idx1] - all_particles->position_y[idx2]);
-    }
+inline double vector(struct particles *all_particles, int index_1, int index_2, char axis)
+{
+    return (axis == X ? (all_particles->position_x[index_1] - all_particles->position_x[index_2])
+                      : all_particles->position_y[index_1] - all_particles->position_y[index_2]);
 }
 
-// Wall time for timing func
-double get_wall_seconds() {
+// Wall seconds function copied from HPPC labs
+inline double get_wall_seconds()
+{
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000;
-    return seconds;
+    return tv.tv_sec + (double)tv.tv_usec / 1000000;
 }
 
 // Main function
-int main(int argc, char const *argv[]) { 
+int main(int argc, char const *argv[])
+{
     // Check there are 6 parameters, but assume the input is correct (program, star_count, filename, timesteps, delta_t, graphics)
-    if (argc != 7) {
-        printf("Error: File requires 5 input parameters");
+    if (argc != 7)
+    {
+        printf("Error: File requires 6 input parameters. Try: \n");
+        printf("./galsim 1000 input_data/ellipse_N_01000.gal 200 1e-5 0 1\n");
         return 1;
     }
 
-    double start_overall_timer = get_wall_seconds(); // Start timer before I/O
-
-    // Assign user input to variables, and declare other variables used. 
-    int star_count = atoi(argv[1]); 
+    // Assign user input to variables, and declare other variables used.
+    star_count = atoi(argv[1]);
     char *file_name = malloc(strlen(argv[2]) + 1);
     strcpy(file_name, argv[2]);
     FILE *file = fopen(file_name, "rb");
-    int time_steps = atoi(argv[3]);
-    double delta_time = atof(argv[4]);
+    time_steps = atoi(argv[3]);
+    delta_time = atof(argv[4]);
     // int graphics = atoi(argv[5]); // Commented out when graphics is not used
-    double gravity = (100.0/star_count);
-    char X = 'x', Y = 'y';
-    int desired_threads = atoi(argv[6]);
-    omp_set_num_threads(desired_threads); // Set the number of threads for OpenMP
+    thread_count = atoi(argv[6]);
+    omp_set_num_threads(thread_count);
+    gravity = (100.0 / star_count);
+    double start_overall_timer = get_wall_seconds(); // Timer for measuring entire process
 
-    // Allocation of memory for particles
+    // Allocate memory for the structure of arrays.
     struct particles all_particles;
-    all_particles.position_x = (double *) malloc(star_count * sizeof(double));
-    all_particles.position_y = (double *) malloc(star_count * sizeof(double));
-    all_particles.mass = (double *) malloc(star_count * sizeof(double));
-    all_particles.velocity_x = (double *) malloc(star_count * sizeof(double));
-    all_particles.velocity_y = (double *) malloc(star_count * sizeof(double));
-    all_particles.brightness = (double *) malloc(star_count * sizeof(double));
-    all_particles.accel_x = (double *) malloc(star_count * sizeof(double));
-    all_particles.accel_y = (double *) malloc(star_count * sizeof(double));
+    all_particles.position_x = malloc(star_count * sizeof(double));
+    all_particles.position_y = malloc(star_count * sizeof(double));
+    all_particles.mass = malloc(star_count * sizeof(double));
+    all_particles.velocity_x = malloc(star_count * sizeof(double));
+    all_particles.velocity_y = malloc(star_count * sizeof(double));
+    all_particles.brightness = malloc(star_count * sizeof(double));
+    all_particles.accel_x = malloc(star_count * sizeof(double));
+    all_particles.accel_y = malloc(star_count * sizeof(double));
 
     // Open the input file and use it to populate the particles in the array of structures
-    for (int index = 0; index < star_count; index++) {
-        if (fread(&all_particles.position_x[index], sizeof(double), 1, file) != 1
-            || fread(&all_particles.position_y[index], sizeof(double), 1, file) != 1
-            || fread(&all_particles.mass[index], sizeof(double), 1, file) != 1
-            || fread(&all_particles.velocity_x[index], sizeof(double), 1, file) != 1
-            || fread(&all_particles.velocity_y[index], sizeof(double), 1, file) != 1
-            || fread(&all_particles.brightness[index], sizeof(double), 1, file) != 1) {
+    for (int index = 0; index < star_count; index++)
+    {
+        if (fread(&all_particles.position_x[index], sizeof(double), 1, file) != 1 ||
+            fread(&all_particles.position_y[index], sizeof(double), 1, file) != 1 ||
+            fread(&all_particles.mass[index], sizeof(double), 1, file) != 1 ||
+            fread(&all_particles.velocity_x[index], sizeof(double), 1, file) != 1 ||
+            fread(&all_particles.velocity_y[index], sizeof(double), 1, file) != 1 ||
+            fread(&all_particles.brightness[index], sizeof(double), 1, file) != 1)
+        {
             fprintf(stderr, "Error reading file\n");
             break;
         }
     }
 
-    // Main loop that runs for the ammount of time steps. Inside the loop are accelleration, velocity, and updated position calculations. 
-    for (int step_iteration = 0; step_iteration < time_steps; step_iteration++) {
-        #pragma omp parallel // Parallelised section
-        {
-            int thread_id = omp_get_thread_num(); // Assign a thread its unique ID from 1-N
-            int total_threads = omp_get_num_threads(); // Variable to hold desired_threads indexed with 0
-            int chunk_size = star_count / total_threads; // Divides the star (particle) count by the threads 
-            int start_index = thread_id * chunk_size; // Assigns a start index for this unqie threads chunk
-            int end_index = (thread_id == total_threads - 1) ? star_count : (thread_id + 1) * chunk_size; // and its end (accounts for a remainder)
-
-            // Calculate acceleration on all particles with equation: Force(i) / Mass(i)
-            for (int index = start_index; index < end_index; index++) {
-                all_particles.accel_x[index] = (force(index, X, &all_particles, star_count, gravity) / all_particles.mass[index]);
-                all_particles.accel_y[index] = (force(index, Y, &all_particles, star_count, gravity) / all_particles.mass[index]);
-            }
-
-            // Ensure all acceleration calculations are done
-            #pragma omp barrier 
-
-            // Calculate Velocity and then position
-            for (int index = start_index; index < end_index; index++) {
-                // Calculate velocity on all particles with equation: Velocity(i) + Delta time * Acceleration(i)
-                all_particles.velocity_x[index] += (delta_time * all_particles.accel_x[index]); // X direction
-                all_particles.velocity_y[index] += (delta_time * all_particles.accel_y[index]); // Y direction
-
-                // Update position of particles with equation: Position(i) + Delta Time * Velocity(i)
-                all_particles.position_x[index] += (delta_time * all_particles.velocity_x[index]); // X directions
-                all_particles.position_y[index] += (delta_time * all_particles.velocity_y[index]); // Y direction
-            }
-            // Ensure all position updates are done
-            #pragma omp barrier 
-        }
-    }
+    // call the function to calculate new positions
+    calculate_particles(&all_particles);
 
     // Declare and populate the output file with the results
     FILE *output = fopen("result.gal", "wb");
-    for (int output_line = 0; output_line < star_count; output_line++) {
-        double position_x = all_particles.position_x[output_line]; 
-        double position_y = all_particles.position_y[output_line]; 
-        double mass = all_particles.mass[output_line]; 
-        double velocity_x = all_particles.velocity_x[output_line]; 
-        double velocity_y = all_particles.velocity_y[output_line]; 
+    for (int output_line = 0; output_line < star_count; output_line++)
+    {
+        double position_x = all_particles.position_x[output_line];
+        double position_y = all_particles.position_y[output_line];
+        double mass = all_particles.mass[output_line];
+        double velocity_x = all_particles.velocity_x[output_line];
+        double velocity_y = all_particles.velocity_y[output_line];
         double brightness = all_particles.brightness[output_line];
         fwrite(&position_x, sizeof(double), 1, output);
         fwrite(&position_y, sizeof(double), 1, output);
@@ -152,18 +244,17 @@ int main(int argc, char const *argv[]) {
         fwrite(&brightness, sizeof(double), 1, output);
     }
 
-    // Close files, free memory, gracefully terminate. 
     fclose(output);
     free(file_name);
-    free(all_particles.position_x);
-    free(all_particles.position_y);
-    free(all_particles.mass);
-    free(all_particles.velocity_x);
-    free(all_particles.velocity_y);
-    free(all_particles.brightness);
     free(all_particles.accel_x);
     free(all_particles.accel_y);
-    //start_overall_timer = get_wall_seconds() - start_overall_timer; // Timer function. Comment out when not using results
-    //printf("\nOverall time: %lf\n\n", start_overall_timer); 
+    free(all_particles.brightness);
+    free(all_particles.mass);
+    free(all_particles.position_x);
+    free(all_particles.position_y);
+    free(all_particles.velocity_x);
+    free(all_particles.velocity_y);
+    start_overall_timer = get_wall_seconds() - start_overall_timer; // Stop timing process for overall runtime
+    printf("\nOverall time: %lf\n\n", start_overall_timer);
     return 0;
 }
